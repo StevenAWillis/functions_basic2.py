2ef9953fb797a878c4e39852696577e4d3262124
Your Name <you@example.com>
2019-07-03 07:07:08 -0500

    first commit

---
+++ functions_basic2.py
@@ -0,0 +1,42 @@
+# # Countdown
+# def a(num):
+
+#     list = []
+#     for i in range(num,-1,-1):
+#         print(i)
+#         list=[i]
+#     return list
+# a(5)
+
+# Print and Return
+
+# def a2(a,b):
+
+#     print(a)
+#     return(b)
+
+# g = a2(5,6)
+# print(g)
+
+# Firsrt Plus Length
+
+# def a3(list):
+
+#     specsum = 0
+
+#     specsum = list[0] + len(list)
+#     print(specsum)
+
+# a3([1,2,3,4,5])
+
+# Values Greater than Second
+
+# def a3(list):
+
+#     specsum = 0
+
+#     specsum = list[0] + len(list)
+#     print(specsum)
+
+# a3([1,2,3,4,5])
+
